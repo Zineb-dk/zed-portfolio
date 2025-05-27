fe235eefdbc987aedc490afcc88c39c183d7acd6
Zineb-dk <zineb.dkier@gmail.com>
2025-05-27 13:10:10 +0100

    Update CloudEffect.tsx

--- src/components/CloudEffect.tsx
+++ src/components/CloudEffect.tsx
@@ -4,11 +4,11 @@ import "../styles/cloud-layer.css";
 const CloudEffect = () => {
   return (
     <div className="floating-clouds">
-      <img src="/images/cloud1.png" className="cloud" style={{ "--i": 1 } as React.CSSProperties} />
-      <img src="/images/cloud2.png" className="cloud" style={{ "--i": 2 } as React.CSSProperties} />
-      <img src="/images/cloud3.png" className="cloud" style={{ "--i": 3 } as React.CSSProperties} />
-      <img src="/images/cloud4.png" className="cloud" style={{ "--i": 4 } as React.CSSProperties} />
-      <img src="/images/cloud5.png" className="cloud" style={{ "--i": 5 } as React.CSSProperties} />
+      <img src="images/cloud1.png" className="cloud" style={{ "--i": 1 } as React.CSSProperties} />
+      <img src="images/cloud2.png" className="cloud" style={{ "--i": 2 } as React.CSSProperties} />
+      <img src="images/cloud3.png" className="cloud" style={{ "--i": 3 } as React.CSSProperties} />
+      <img src="images/cloud4.png" className="cloud" style={{ "--i": 4 } as React.CSSProperties} />
+      <img src="images/cloud5.png" className="cloud" style={{ "--i": 5 } as React.CSSProperties} />
     </div>
   );
 };
